e68069bc0ea2a09c3d824813c84a6706577f0640
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-01-18 14:54:34 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.25

---
+++ src/ProfileCanvas.jl
@@ -0,0 +1,5 @@
+module ProfileCanvas
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ProfileCanvas
+using Test
+
+@testset "ProfileCanvas.jl" begin
+    # Write your tests here.
+end
